d978055ecc31533b099cf6281fe75bb4cec389c9
Scott Austin <scott.austin@stryker.com>
2016-02-02 07:59:35 -0500

    Initial commit
    Signed-off-by: Scott Austin <scott.austin@stryker.com>

---
+++ Intervals/Constants.swift
@@ -0,0 +1,48 @@
+//
+//  Constants.swift
+//  Intervals
+//
+//  Created by Scott Austin on 2/1/16.
+//  Copyright © 2016 europaSoftware. All rights reserved.
+//
+
+import Foundation
+
+let kDefaultRunTime: NSTimeInterval = 120
+let kDefaultWalkTime: NSTimeInterval = 60
+
+/*
+* Delay
+* Runs x code after a delay
+*/
+
+func delay(delay:Double, closure:()->())
+{
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
+}
+
+/*
+* Performs a countdown.
+* Method performs countdownClosure at each second, and finalClosure when the countdown is finished.
+*/
+func countdown(seconds: Int,
+    eachSecondAction countdownClosure: (timeIndex: Int) -> (),
+    finalAction finalClosure: () -> ())
+{
+    for index in 1...(seconds - 1)
+    {
+        delay(Double(index))
+            {
+                countdownClosure(timeIndex: index)
+        }
+    }
+    delay(Double(seconds))
+        {
+            finalClosure()
+    }
+}
--- Intervals/ViewController.swift
+++ Intervals/ViewController.swift
@@ -6,23 +6,64 @@
 //  Copyright © 2016 europaSoftware. All rights reserved.
 //
 
+//TODO: Fix weird animation issue on reset.
+//TODO: Add running/walking status label
+//TODO: Support running in the background
+//TODO: Partner watch app
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TimerLabelDelegate
+{
 
     var startTime: NSDate?
     var totalTime = 25200
     var runInterval = 180.0
     var walkInterval = 60.0
     
-    @IBOutlet var runLabel: UILabel!
-    @IBOutlet var walkLabel: UILabel!
-    @IBOutlet var startButton: UIButton!
+    @IBOutlet var runLabel: UILabel?
+    @IBOutlet var walkLabel: UILabel?
+    @IBOutlet var overallLabel: UILabel?
+    @IBOutlet var startButton: UIButton?
+    @IBOutlet var resetButton: UIButton?
+    
+    @IBOutlet var runUserInputTextField: UITextField?
+    @IBOutlet var walkUserInputTextField: UITextField?
+    
+    @IBOutlet var userInputGroup: UIStackView?
+    @IBOutlet var timerGroup: UIStackView?
+    
+    var userRunTime = kDefaultRunTime
+    var userWalkTime = kDefaultWalkTime
+
+    var overallTimer: TimerLabel?
+    var runTimer: TimerLabel?
+    var walkTimer: TimerLabel?
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.runTimer = TimerLabel(label: self.runLabel, timerType: .Timer)
+        self.runTimer?.timeFormat = "mm:ss"
+        self.runTimer?.timerDelegate = self
+        self.runTimer?.setCountDownTime(self.userRunTime)
+
+        self.walkTimer = TimerLabel(label: self.walkLabel, timerType: .Timer)
+        self.walkTimer?.timeFormat = "mm:ss"
+        self.walkTimer?.timerDelegate = self
+        self.walkTimer?.setCountDownTime(self.userWalkTime)
+        
+        self.overallTimer = TimerLabel(label: self.overallLabel, timerType: .Stopwatch)
+        self.overallTimer?.timeFormat = "hh:mm:ss"
+        self.overallTimer?.timerDelegate = self
+        
+        self.resetButton?.hidden = true
+        
+        self.timerGroup?.hidden = true
+
     }
 
     override func didReceiveMemoryWarning()
@@ -31,90 +72,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func deceaseWalkInterval(sender: UIButton)
-    {
-        self.walkInterval -= 60.0
-        self.updateWalkLabel()
-    }
-    
-    @IBAction func increaseWalkInterval(sender: UIButton)
-    {
-        self.walkInterval += 60.0
-        self.updateWalkLabel()
-    }
-    
-    @IBAction func decreaseRunInterval(sender: UIButton)
-    {
-        self.runInterval -= 60.0
-        self.updateRunLabel()
-    }
-    
-    @IBAction func increaseRunInterval(sender: UIButton)
-    {
-        self.runInterval += 60
-        self.updateRunLabel()
-    }
-    
-    func updateRunLabel()
-    {
-        let minutes = Int(self.runInterval / 60.0)
-        let seconds = Int(self.runInterval % 60)
-        
-        var minutesString = "\(minutes)"
-        if minutesString.characters.count == 1
-        {
-            minutesString = "0" + minutesString
-        }
-        var secondsString = "\(seconds)"
-        if secondsString.characters.count == 1
-        {
-            secondsString = "0" + secondsString
-        }
-        
-        self.runLabel.text = "\(minutesString):\(secondsString)"
-    }
-    
-    func updateWalkLabel()
-    {
-        let minutes = Int(self.walkInterval / 60.0)
-        let seconds = Int(self.walkInterval % 60)
-        
-        var minutesString = "\(minutes)"
-        if minutesString.characters.count == 1
-        {
-            minutesString = "0" + minutesString
-        }
-        var secondsString = "\(seconds)"
-        if secondsString.characters.count == 1
-        {
-            secondsString = "0" + secondsString
-        }
-        
-        self.walkLabel.text = "\(minutesString):\(secondsString)"
-    }
-
-    
     @IBAction func startWasPressed()
     {
-        if self.startButton.titleLabel?.text == "Start"
+        //If we've already pushed start...
+        if self.overallTimer!.counting
         {
-            var index = 0
-            while index < self.totalTime
-            {
-                self.scheduleNotifications(index, runReminder: true)
-                index += Int(self.runInterval)
-                self.scheduleNotifications(index, runReminder: false)
-                index += Int(self.walkInterval)
-            }
-            self.startButton.titleLabel?.text = "End"
+            self.overallTimer?.pause()
+            self.runTimer?.pause()
+            self.walkTimer?.pause()
+            
+            self.startButton?.setBackgroundImage(UIImage(contentsOfFile: NSBundle.mainBundle().pathForResource("startButton", ofType: "png")!), forState: .Normal)
         }
         else
         {
-            self.clearNotifications()
-            self.startButton.titleLabel?.text = "Start"
+            UIView.animateWithDuration(0.5)
+            {
+                self.timerGroup?.hidden = false
+                self.userInputGroup?.hidden = true
+                self.resetButton?.hidden = false
+            }
+            
+            self.overallTimer?.start()
+            self.runTimer?.start()
+            self.startButton?.setBackgroundImage(UIImage(contentsOfFile: NSBundle.mainBundle().pathForResource("pauseButton", ofType: "png")!), forState: .Normal)
+
+        }
+    }
+    
+    @IBAction func resetWasPressed(sender: UIButton)
+    {
+        if self.overallTimer!.counting
+        {
+            self.startButton?.setBackgroundImage(UIImage(contentsOfFile: NSBundle.mainBundle().pathForResource("startButton", ofType: "png")!), forState: .Normal)
         }
         
+        self.overallTimer?.pause()
+        self.runTimer?.pause()
+        self.walkTimer?.pause()
         
+        self.overallTimer?.reset()
+        self.runTimer?.reset()
+        self.walkTimer?.reset()
+        
+        UIView.animateWithDuration(0.5)
+        {
+            self.timerGroup?.hidden = true
+            self.userInputGroup?.hidden = false
+            self.resetButton?.hidden = true
+        }
+
     }
 
     func scheduleNotifications(timeFromNow: Int, runReminder: Bool)
@@ -158,5 +164,65 @@ class ViewController: UIViewController {
 
     }
 
+    //MARK: - UITextFields
+    @IBAction func runTimeWasUpdated(sender: UITextField)
+    {
+        self.userRunTime = NSTimeInterval((sender.text! as NSString).integerValue * 60)
+        self.runTimer?.setCountDownTime(self.userRunTime)
+    }
+    
+    @IBAction func walkTimeWasUpdated(sender: UITextField)
+    {
+        self.userWalkTime = NSTimeInterval((sender.text! as NSString).integerValue * 60)
+        self.walkTimer?.setCountDownTime(self.userWalkTime)
+    }
+    
+    //MARK: - Timer Delegate
+    
+    func timerLabel(timerLabel: TimerLabel, countingTo time: NSTimeInterval, timerType: TimerLabelType)
+    {
+        
+    }
+    
+    func timerLabel(timerLabel: TimerLabel, customTextToDisplayAtTime time: NSTimeInterval) -> String?
+    {
+        return nil
+    }
+    
+    func timerLabel(timerLabel: TimerLabel, finishedCountDownTimerWithTime countTime: NSTimeInterval)
+    {
+        if timerLabel == self.runTimer
+        {
+            self.walkTimer?.start()
+            self.runTimer?.reset()
+            self.runTimer?.setCountDownTime(self.userRunTime)
+            
+            let alert = UIAlertController(title: "Switch!", message: "Start walking!", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            
+            self.presentViewController(alert, animated: true)
+            {
+                delay(3)
+                {
+                    alert.dismissViewControllerAnimated(true, completion: nil)
+                }
+            }
+        }
+        else if timerLabel == self.walkTimer
+        {
+            self.runTimer?.start()
+            self.walkTimer?.reset()
+            self.walkTimer?.setCountDownTime(self.userWalkTime)
+
+        }
+    }
+    
+    @IBAction func viewTapped(sender: AnyObject)
+    {
+        self.walkUserInputTextField?.resignFirstResponder()
+        self.runUserInputTextField?.resignFirstResponder()
+    }
+
+    
 }
 
